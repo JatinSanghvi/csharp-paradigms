d77346ac284fafe9dfedbb634ea2eddae2ba773a
Jatin Sanghvi <20547963+JatinSanghvi@users.noreply.github.com>
2020-02-10 23:15:16 -0800

    Add files from 'Functional Programming' module

---
+++ Paradigms/FunctionalProgramming/RetryingExecution.cs
@@ -0,0 +1,49 @@
+﻿namespace Paradigms.FunctionalProgramming
+{
+    using System;
+    using System.Net;
+
+    internal static class RetryingExecution
+    {
+        public static void Main(string[] args)
+        {
+            var client = new WebClient();
+            Download(client, "https://jsonplaceholder.typicode.com/users");
+            Download(client, "https://api.github.com");
+        }
+
+        private static void Download(WebClient client, string address)
+        {
+            Console.WriteLine($"Downloading page: {address}.");
+            Func<string> download = () => client.DownloadString(address);
+            download.WithRetry();
+        }
+
+        private static T WithRetry<T>(this Func<T> function)
+        {
+            int attempt = 1;
+
+            while (true)
+            {
+                Console.WriteLine($"Attempt: {attempt}");
+
+                try
+                {
+                    T result = function();
+                    Console.WriteLine("Execution succeeded.");
+                    return result;
+                }
+                catch
+                {
+                    if (attempt == 3)
+                    {
+                        Console.WriteLine("Execution failed.");
+                        throw;
+                    }
+
+                    attempt += 1;
+                }
+            }
+        }
+    }
+}
---
+++ Paradigms/FunctionalProgramming/TimingExecution.cs
@@ -0,0 +1,45 @@
+﻿namespace Paradigms.FunctionalProgramming
+{
+    using System;
+    using System.Diagnostics;
+
+    internal static class TimingExecution
+    {
+        public static void Main(string[] args)
+        {
+            TimeSpan elapsed = GetExecutionTime(() =>
+            {
+                int number = 3;
+                int exponent = 15;
+                Console.WriteLine($"{number}^{exponent} = {SlowExponent(number, exponent)}");
+            });
+
+            Console.WriteLine($"Execution Time: {elapsed.Milliseconds} mSec.");
+        }
+
+        private static int SlowExponent(int number, int exponent)
+        {
+            if (exponent == 0)
+            {
+                return 1;
+            }
+
+            int result = 0;
+
+            for (int count = 0; count < number; count++)
+            {
+                result += SlowExponent(number, exponent - 1);
+            }
+
+            return result;
+        }
+
+        private static TimeSpan GetExecutionTime(Action action)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action();
+            return stopwatch.Elapsed;
+        }
+    }
+}
